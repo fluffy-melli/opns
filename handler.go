c815cafcd8ad8de8e21095dfe06cf31f1852b003
shibaisdog <kimjaehoon5142@gmail.com>
2024-09-01 09:58:49 +0900

    update
    add (Handler) Respond

--- handler.go
+++ handler.go
@@ -1,8 +1,24 @@
 package opns
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 type Handler struct {
 	Interaction *discordgo.InteractionCreate
 	Client      *discordgo.Session
+}
+
+func (h *Handler) Respond(Content string) {
+	err := h.Client.InteractionRespond(h.Interaction.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: Content,
+		},
+	})
+	if err != nil {
+		fmt.Println("error responding: ", err)
+	}
 }
